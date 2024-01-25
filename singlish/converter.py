26ca63dd01eed177cc98526d862ea9737e1a54dc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:07:38 +0530

    Update converter.py

--- singlish/converter.py
+++ singlish/converter.py
@@ -1,2 +1,11 @@
+import requests
+
 def singlish_to_english(singlish_text: str) -> str:
-    pass
+    url = "https://easysinhalaunicode.com/api/convert"
+    data = {"data": singlish_text}
+    response = requests.post(url, data=data)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return "Error: Unable to convert text"

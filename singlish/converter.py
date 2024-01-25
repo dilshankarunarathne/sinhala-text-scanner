f69b5f802adc2d773017bbcd486f9803857caa3a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 21:56:21 +0530

    bugfix 406

--- singlish/converter.py
+++ singlish/converter.py
@@ -4,14 +4,17 @@ import requests
 def singlish_to_english(singlish_text: str) -> str:
     url = "https://easysinhalaunicode.com/api/convert"
     data = {"data": singlish_text}
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
 
     response = requests.post(url, data=data, headers=headers)
 
     if response.status_code == 200:
         print("Singlish translation: ", response.text)
         return response.text
-    else: 
+    else:
         print(response.text)
         return "Error: Unable to convert text"
 

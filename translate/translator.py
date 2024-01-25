e2bbd65e9f022aefcbe864b46ba176c6ce9235e3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:02:11 +0530

    Update translator.py

--- translate/translator.py
+++ translate/translator.py
@@ -18,21 +18,3 @@ def translate():
         output = translator.translate(lang_1, dest=cl)
         text_entry2.insert('end', output.text)
 
-
-
-
-text_entry1 = Text(frame1, width=20, height=7, borderwidth=5, relief=RIDGE, font=('verdana', 15))
-text_entry1.place(x=10, y=100)
-
-text_entry2 = Text(frame1, width=20, height=7, borderwidth=5, relief=RIDGE, font=('verdana', 15))
-text_entry2.place(x=300, y=100)
-
-btn1 = Button(frame1, command=translate, text="Translate", relief=RAISED, borderwidth=2, font=('verdana', 10, 'bold'),
-              bg='#248aa2', fg="white", cursor="hand2")
-btn1.place(x=185, y=300)
-
-btn2 = Button(frame1, command=clear, text="Clear", relief=RAISED, borderwidth=2, font=('verdana', 10, 'bold'),
-              bg='#248aa2', fg="white", cursor="hand2")
-btn2.place(x=300, y=300)
-
-root.mainloop()

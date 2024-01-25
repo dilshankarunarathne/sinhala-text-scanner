b4458405ddafb330ad9bdacc148201aa9fe803bd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 17:57:07 +0530

    Update convert.py

--- routes/convert.py
+++ routes/convert.py
@@ -11,5 +11,11 @@ router = APIRouter(
 async def logout(
         image: File(...),
 ):
-    
+    if image and image.content_type != "image/jpeg":
+        return {300: {"description": "Only jpeg images are supported"}}
+        diseases = get_diseases(None, bmi)
+    else:
+        contents = await image.read()
+        nparray = np.fromstring(contents, np.uint8)
+        img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
     return {"message": "Image uploaded..."}

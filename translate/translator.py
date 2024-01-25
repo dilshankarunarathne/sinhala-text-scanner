c6e3c3658ef710198ee13972a1f7cd3fa95a833f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:02:43 +0530

    Update translator.py

--- translate/translator.py
+++ translate/translator.py
@@ -5,14 +5,14 @@ from googletrans import Translator
 
 
 def translate():
-    lang_1 = text_entry1.get("1.0", "end-1c")
+    si_text = text_entry1.get("1.0", "end-1c")
     cl = choose_language.get()
 
-    if lang_1 == '':
+    if si_text == '':
         messagebox.showerror('Language Translator', 'Enter to the translate')
     else:
         text_entry2.delete(1.0, 'end')
         translator = Translator()
-        output = translator.translate(lang_1, dest=cl)
+        output = translator.translate(si_text, dest=cl)
         text_entry2.insert('end', output.text)
 

00b6a7aa27ed2700dc629cb2af588a6b24d96f9d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 17:26:47 +0530

    Update convert.py

--- routes/convert.py
+++ routes/convert.py
@@ -5,12 +5,6 @@ from typing import Annotated
 from fastapi import APIRouter, Form, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from models.user_model import UserInDB
-
-from convert.authorize import authenticate_user, oauth2_scheme
-from convert.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, blacklist_token
-from services.user_service import user_exists, get_next_avail_id, add_new_user
-
 """
     API router for auth endpoint
     

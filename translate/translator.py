0f00a6eb6681c3ca3c6b18b00c2700da5c0d20ef
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 22:00:57 +0530

    Update translator.py

--- translate/translator.py
+++ translate/translator.py
@@ -4,4 +4,5 @@ translator = Translator()
 
 
 def translate_text(si_text: str) -> str:
-    return translator.translate(si_text, dest='English')
+    text = translator.translate(si_text, dest='English')
+    return text.text

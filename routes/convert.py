f692ae5406d71191704b8e8acf5a7ec6d38a4bdb
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 17:57:46 +0530

    Update convert.py

--- routes/convert.py
+++ routes/convert.py
@@ -20,5 +20,7 @@ async def logout(
         contents = await image.read()
         nparray = np.fromstring(contents, np.uint8)
         img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
+
         
+
     return {"message": "Image uploaded..."}

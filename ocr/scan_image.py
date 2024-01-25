4c3c9ce7a49cc7dc8d2b76b254fa8dd2599a7a7b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:16:37 +0530

    Update scan_image.py

--- ocr/scan_image.py
+++ ocr/scan_image.py
@@ -3,4 +3,9 @@ import numpy as np
 from keras.models import load_model
 import pickle
 
-
+# Load the model
+model = load_model('sinhala_model_final.h5')
+# Load the LabelBinarizer
+LB = pickle.load(open('label_binarizer.pkl', 'rb'))
+# Load the image
+img = cv2.imread('test.jpg',0)

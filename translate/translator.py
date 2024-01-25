ddad5b77aded72d166e2d0ee142415b1125c027f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:03:12 +0530

    Update translator.py

--- translate/translator.py
+++ translate/translator.py
@@ -6,8 +6,6 @@ from googletrans import Translator
 
 def translate(si_text: str):
     cl = choose_language.get()
-
-    text_entry2.delete(1.0, 'end')
     translator = Translator()
     output = translator.translate(si_text, dest=cl)
     text_entry2.insert('end', output.text)

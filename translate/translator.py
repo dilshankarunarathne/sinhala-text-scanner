ac42cadf5b493143364ee03df7ac1a7df8eb0c7f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:03:15 +0530

    Update translator.py

--- translate/translator.py
+++ translate/translator.py
@@ -3,10 +3,10 @@ from tkinter import messagebox
 
 from googletrans import Translator
 
+translator = Translator()
 
 def translate(si_text: str):
     cl = choose_language.get()
-    translator = Translator()
     output = translator.translate(si_text, dest=cl)
     text_entry2.insert('end', output.text)
 

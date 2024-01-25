6b16cb02d4f0cc9f6f44154534a497f22407b91e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:07:50 +0530

    Update converter.py

--- singlish/converter.py
+++ singlish/converter.py
@@ -2,6 +2,7 @@ import requests
 
 url = "https://easysinhalaunicode.com/api/convert"
 
+
 def singlish_to_english(singlish_text: str) -> str:
     data = {"data": singlish_text}
     response = requests.post(url, data=data)

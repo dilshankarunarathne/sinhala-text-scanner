679a1cbbf6831125217710b3de2a6019591a1725
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:19:44 +0530

    Update scan_image.py

--- ocr/scan_image.py
+++ ocr/scan_image.py
@@ -16,3 +16,17 @@ def scan_image(img):
 
     predicted_label = LB.inverse_transform(prediction)
     return predicted_label
+
+
+def sort_contours(cnts, method="left-to-right"):
+    reverse = False
+    i = 0
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+    key=lambda b:b[1][i], reverse=reverse))
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)

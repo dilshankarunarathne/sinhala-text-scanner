3f0575caeea0a78746175752f79d98b6e3f805e8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-25 18:18:02 +0530

    Update scan_image.py

--- ocr/scan_image.py
+++ ocr/scan_image.py
@@ -3,23 +3,16 @@ import numpy as np
 from keras.models import load_model
 import pickle
 
-# Load the model
 model = load_model('sinhala_model_final.h5')
-# Load the LabelBinarizer
 LB = pickle.load(open('label_binarizer.pkl', 'rb'))
-# Load the image
-img = cv2.imread('test.jpg',0)
 
 
 def scan_image(img):
-    # Preprocess the image
     img = cv2.resize(img, (80, 80))
     img_array = np.expand_dims(img, axis=0)
     img_array = img_array / 255.0
 
-    # Make a prediction
     prediction = model.predict(img_array)
 
-    # Get the class label
     predicted_label = LB.inverse_transform(prediction)
     return predicted_label
